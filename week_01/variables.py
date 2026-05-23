07ad6d43026c6d23e86305adc88d9526b9038874
Dan <butiongillidan@gmail.com>
2026-05-23 11:09:18 +0800

    docs: update variables.py

--- week_01/variables.py
+++ week_01/variables.py
@@ -1,1 +1,8 @@
+# storing my personal info
+name = "dandan"        # my name
+subject = "comscie"    # my course/subject
+math_operation = "square"  # type of math operation
 
+print(name)
+print(subject)
+print(math_operation)
