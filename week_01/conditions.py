6881b9345da76d029d5424e1f595c3f02ffb0227
Dan <butiongillidan@gmail.com>
2026-05-25 20:04:09 +0800

    docs: Add conditional statements for score and age checks

--- week_01/conditions.py
+++ week_01/conditions.py
@@ -1,1 +1,21 @@
+# 1_LESSON - CONDITIONS
+score = 92
 
+if score >= 90:
+    print("Outstanding!")
+elif score >= 85:
+    print("Great job!")
+elif score >= 78:
+    print("Good!")
+else:
+    print("Keep practicing!")
+
+
+# 2_LESSON - CONDITIONS
+age = 19
+has_id = True
+
+if age >= 18 and has_id == True:
+    print("You can enter!")
+else:
+    print("You cannot enter!")
