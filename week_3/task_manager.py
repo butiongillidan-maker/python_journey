9094d3fd2b362d719f1974b6b2017616166e139e
Dan <butiongillidan@gmail.com>
2026-07-11 11:09:53 +0800

    docs: Refactor task manager to load and save tasks from file

--- week_3/task_manager.py
+++ week_3/task_manager.py
@@ -1,17 +1,23 @@
-task_list = [
-    "going to sleep",
-    "eat breakfast",
-    "do a coding in python",
-    "play videogames",
-    "study operating systems in cisco"
-]
+import os
 
-# 1. Start the infinite loop here
+if os.path.exists("task.txt"):
+    with open("task.txt", "r")as file:
+        task_list = [line.strip() for line in file]
+
+else:
+    task_list = []
+
+def save_task():
+    with open("task.txt", "w")as file:
+        for task in task_list:
+            file.write(task)
+            file.write("\n")
+
+
 while True:
     print("1. View Task\n 2. Add a task\n 3. Complete / Remove task\n 4. Exit")
     choices = int(input("My choice: "))
 
-    # 2. All of this must be indented inside the while loop
     if choices == 1:
         if not task_list:
             print("task is empty! no task is scheduled")
@@ -19,11 +25,13 @@ while True:
             addition = 0
             for task in task_list:
                 addition = addition + 1
-                print(f"{addition}: {task}")
+                print(f"{addition}: {task}", "\n")
                 
     elif choices == 2:
         new_task = input("Add a New Task: ")
         task_list.append(new_task)
+        save_task()
+        
         print("task added successfully!")
         
     elif choices == 3:
@@ -36,6 +44,7 @@ while True:
                 print(f"{addition}: {task}")
             completed_num = int(input("Enter the number of task you completed: "))
             removed_task = task_list.pop(completed_num - 1)
+            save_task()
             print(f"The {removed_task} is complete and removed!")
             
     elif choices == 4:
