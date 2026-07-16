f8be8693707e1aab4f611cb4144fad3a41b65ac8
Dan <butiongillidan@gmail.com>
2026-07-17 06:46:37 +0800

    Refactor log alert generation into a list comprehension

--- week_3/LogFile_Auditor.py
+++ week_3/LogFile_Auditor.py
@@ -6,19 +6,15 @@ raw_logs = [
     "INFO: User logged out"
 ]
 
-alerts = []
 
-for index, raw_log in enumerate(raw_logs, start=1):
-    if "ERROR" in raw_log or "CRITICAL" in raw_log:
-        formatted_line = f"line {index}: {raw_log} That find"
-        print(formatted_line)
-        
-        alerts.append(formatted_line)
-
+alerts = [
+    f"line {index}: {raw_log} That find" 
+    for index, raw_log in enumerate(raw_logs, start=1) 
+    if "ERROR" in raw_log or "CRITICAL" in raw_log
+]
 
 sorted_alerts = sorted(alerts)
 
 print("\nSorted Alerts:")
 for alert in sorted_alerts:
     print(alert)
-    
