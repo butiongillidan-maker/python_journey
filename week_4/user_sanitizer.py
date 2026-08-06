f8fa73b585306e315f4fedf674655d8f0388a9eb
Dan <butiongillidan@gmail.com>
2026-08-07 02:09:15 +0800

    docs: Update user sanitizer to handle user data correctly

--- week_4/user_sanitizer.py
+++ week_4/user_sanitizer.py
@@ -1,19 +1,31 @@
 import json
 
-with open("raw_user.json", "r")as f:
-    json.load(f)
+with open("raw_users.json", "r")as f:
+    users = json.load(f)
 
     active_users = []
     security_alerts = []
 
     for user in users:
-        if user["active"] == True and user["email_verified"] == True
-        user["status"] = "Verified"
-        active_users.append(user)
+        if user["active"] == True and user["email_verified"] == True:
+            user["status"] = "Verified"
+            active_users.append(user)
 
+                    
         if user["failed_logins"] > 10:
             security_alerts.append({"username": user["username"], "failed_logins":
             user["failed_logins"]})
+
+            with open("sannitized.json", "w")as f:
+                json.dump(active_users, f, indent = 4) 
+
+            with open("security_audit", "w")as f:
+                json.dump(security_alerts, f, indent = 4)
+
+
+            print("Sanitation Commplete!")
+            print(f"Active users saved: {len(active_users)}")
+            print(f"Security Alerts Generated: {len(security_alerts)}")
 
             
              
