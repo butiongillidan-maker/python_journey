8a3cc0446cb4d214924c101b050be2957de7d42c
Dan <butiongillidan@gmail.com>
2026-08-14 19:41:39 +0800

    docs: Fix syntax error and add account deactivation method

--- week_4/User_Account.py
+++ week_4/User_Account.py
@@ -1,7 +1,12 @@
 class UserAccount:
-    def __init__(self, username, role)
+    def __init__(self, username, role):
         self.username = username
         self.role = role
-        self.is_active = True 
+        self.is_active = True
 
-    
+    def get_details(self):
+        return f"this {self.username}, and his {self.role}, - {self.is_active}"
+
+    def deactivate(self):
+        set.is_active = False
+        print(f"Your account {self.username} has been deactivated.")
