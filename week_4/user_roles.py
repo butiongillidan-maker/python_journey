bfb9ff8ccfde91e45e7e8ca7e90d25465ac76c12
Dan <butiongillidan@gmail.com>
2026-08-16 14:05:30 +0800

    docs: Add unfinished User and Admin classes for user management

---
+++ week_4/user_roles.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+    def get_info(self):
+        return f"User : {self.username} - {self.email}"
+    
+class Admin(User):
+    def Delete_user(self):
+
+    
+   
+       
+
+
+        
