a48e89b7dc5ca7c8c391246afdae643043e50ad4
Dan <butiongillidan@gmail.com>
2026-08-16 15:05:37 +0800

    docs: Add Delete_user method to Admin class

--- week_4/user_roles.py
+++ week_4/user_roles.py
@@ -6,9 +6,14 @@ class User:
         return f"User : {self.username} - {self.email}"
     
 class Admin(User):
-    def Delete_user(self):
+    def Delete_user(self, target_user):
+        print(f"Admin {self.username} deleted user {target_user}.")
 
-    
+standard_user = User("Boss DanDan", "Dan@admin.com")
+admin_user = Admin("illidan", "illidan@stormrage.com")
+print(admin_user.get_info())
+print(standard_user.get_info())
+admin_user.Delete_user("Boss DanDan")
    
        
 
