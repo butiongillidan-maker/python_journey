aaa0cad9fb58bca6a58bb47993b3709f2afb2eb6
Dan <butiongillidan@gmail.com>
2026-08-20 17:37:10 +0800

    docs: Fix change_password method for correct validation

--- week_4/user_security.py
+++ week_4/user_security.py
@@ -3,13 +3,16 @@ class SecureUser:
         self.username = username
         self.__password = password
 
-        def change_password(self, old_password, new_password):
-            old_password = self.__password
-            print("password updated successfully!")
-            if not isinstance(old_password, self.__password):
-                print("Error: incorrect old password access denied")
+    def change_password(self, old_password, new_password):
+            if old_password == self.__password:
+                self.__password = new_password
+                print("password updated successfully!")
+            else:
+                print("ERROR: YOU INPUT THE WRONG PASSWORD!")
 
-   
+
 user = SecureUser("Dan_sec", "supermegasecret123")
 
-print(user.__password)
+user.change_password("wrongpass", "newpassword123")
+
+user.change_password("supermegasecret123", "newpassword123")
